d82487140dac4906f65e47b1c9435f5e172b6d8d
emmelleppi <mlp.perego@gmail.com>
2021-04-15 11:04:19 +0200

    v12

--- src/MultiSlider.js
+++ src/MultiSlider.js
@@ -3,14 +3,9 @@ import { useSprings, animated, useSpring } from "react-spring";
 import { useDrag } from "react-use-gesture";
 import useResizeObserver from "use-resize-observer";
 
-function easeInOutExpo(x) {
-  return x === 0
-    ? 0
-    : x === 1
-    ? 1
-    : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
-    : (2 - Math.pow(2, -20 * x + 10)) / 2;
-  }
+  function easeInOutExpo(x) {
+    return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+    }
 
 const defaultProps = {
   draggedScale: 1,
